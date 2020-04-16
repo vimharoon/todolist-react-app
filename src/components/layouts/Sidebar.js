86445dc8ee0bbaab4272f493fd1eda763ce626f4
mac <mac@mac.local>
2020-04-16 13:45:12 +0200

    setup hooks to fetch data from firebase

--- src/components/layouts/Sidebar.js
+++ src/components/layouts/Sidebar.js
@@ -1,3 +1,36 @@
 import React from 'react'
 
-export const Sidebar = () => <p>I am a sidebar</p>
+export const Sidebar = () => {
+  return (
+    <div className="sidebar" data-testid="sidebar">
+      <ul className="sidebar__generic">
+        <li>
+          <span>
+            <i data-eva="inbox"></i>
+          </span>
+          <span>Inbox</span>
+        </li>
+        <li>
+          <span>
+            <i data-eva="calendar"></i>
+          </span>
+          <span>Today</span>
+        </li>
+        <li>
+          <span>
+            <i data-eva="pantone"></i>
+          </span>
+          <span>Next 7 days</span>
+        </li>
+      </ul>
+      <div className="sidebar__middle">
+        <span>
+          <i data-eva="chevron-down"></i>
+        </span>
+        <h2>Projects</h2>
+      </div>
+      <ul className="sidebar__projects">Projects will be here</ul>
+      Add project component here!
+    </div>
+  )
+}
